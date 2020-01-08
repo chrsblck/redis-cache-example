2d26343282c346920b263a66149d4642fe0ffde7
Chris Black <chrisbblack@gmail.com>
2020-01-07 16:37:44 -0800

    Fix Test class name

--- src/test/kotlin/com/example/rediscacheexample/RedisCacheExampleApplicationTests.kt
+++ src/test/kotlin/com/example/rediscacheexample/RedisCacheExampleApplicationTests.kt
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
-class SoRedisCacheManagerApplicationTests {
+class RedisCacheExampleApplicationTests {
 
 	@Test
 	fun contextLoads() {
